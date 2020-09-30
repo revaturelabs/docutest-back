864a2a961d06658c63362556bce82b49249dba41
bach-tran <bach_tran@outlook.com>
2020-09-30 13:20:03 -0500

    Controller with exploratory output of swagger files

--- src/main/java/com/revature/controllers/SwaggerfileController.java
+++ src/main/java/com/revature/controllers/SwaggerfileController.java
@@ -2,6 +2,7 @@ package com.revature.controllers;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -10,14 +11,12 @@ import io.swagger.models.HttpMethod;
 import io.swagger.models.Model;
 import io.swagger.models.Operation;
 import io.swagger.models.Path;
-import io.swagger.models.Response;
 import io.swagger.models.Swagger;
 import io.swagger.models.parameters.BodyParameter;
+import io.swagger.models.parameters.HeaderParameter;
 import io.swagger.models.parameters.Parameter;
-import io.swagger.models.properties.ArrayProperty;
+import io.swagger.models.parameters.PathParameter;
 import io.swagger.models.properties.Property;
-import io.swagger.models.properties.RefProperty;
-import io.swagger.models.refs.RefFormat;
 import io.swagger.parser.SwaggerParser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -41,7 +40,7 @@ public class SwaggerfileController {
     
     @PostMapping("/upload")
     public ResponseEntity<Void> uploadSwaggerFile(@RequestParam("file") MultipartFile file) throws IOException {
-        System.out.println(file.getOriginalFilename());
+        System.out.println("Filename: " + file.getOriginalFilename() + "\n");
         
         InputStream jsonStream = file.getInputStream();
         
@@ -49,9 +48,6 @@ public class SwaggerfileController {
         JsonNode node = mapper.readTree(jsonStream);
         
         Swagger swag = new SwaggerParser().read(node);
-        
-        swag.getDefinitions();
-        System.out.println(swag.getInfo().getDescription());
         
         for (Map.Entry<String, Path> entry : swag.getPaths().entrySet()) {
             System.out.println(entry.getKey());
@@ -63,23 +59,47 @@ public class SwaggerfileController {
     
     private static void printOperations(Swagger swag, Map<HttpMethod, Operation> operationMap) {
         for (Map.Entry<HttpMethod, Operation> op : operationMap.entrySet()) {
-            System.out.println(op.getKey() + " - " + op.getValue().getOperationId());
+            System.out.println("HTTP Method: " + op.getKey() + " - " + op.getValue().getOperationId());
+            
             System.out.println("Parameters: ");
-            for (Parameter p : op.getValue().getParameters()) {
-                System.out.println(p.getName() + ", ");
+            printParameters(swag, op.getValue().getParameters());
+        }
+    }
+    
+    private static void printParameters(Swagger swag, List<Parameter> parameters) {
+        for (Parameter p : parameters) {
+            System.out.println("- " + p.getName() + " (in " + p.getIn() + ")");
+            if (p instanceof PathParameter) {
+                System.out.println("    Type: " + ((PathParameter) p).getType());
+            } else if (p instanceof HeaderParameter) {
+                System.out.println("    Type: " + ((HeaderParameter) p).getType());
+                System.out.println("    Default value: " + ((HeaderParameter) p).getDefaultValue());
+            }
+            
+            if (p instanceof BodyParameter) {
+                BodyParameter bp = (BodyParameter) p;
+                Model schema = bp.getSchema();
                 
-                if (p instanceof BodyParameter) {
-                    BodyParameter bp = (BodyParameter) p;
-                    Model schema = bp.getSchema();
-                    Map<String, Property> map = schema.getProperties();
-                    
-                    
-                    System.out.println(schema.getReference());
+                String reference = schema.getReference();
+                System.out.println("    Schema full reference: " + reference);
+                
+                String simpleRef = reference.replaceFirst(".*/", "");
+                System.out.println("    Simple ref: " + simpleRef);
+                
+                Map<String, Model> definitions = swag.getDefinitions();
+                Model definition = definitions.get(simpleRef);
+                Map<String, Property> bodyProperties = definition.getProperties();
+                
+                for (Map.Entry<String, Property> entry : bodyProperties.entrySet()) {
+                    System.out.println("    Property: ");
+                    System.out.println("        " + "body property: " + entry.getKey());
+                    System.out.println("        " + "body type: " + entry.getValue().getType());
                 }
                 
             }
-            System.out.println();
+            
         }
+        System.out.println();
     }
     
 }
